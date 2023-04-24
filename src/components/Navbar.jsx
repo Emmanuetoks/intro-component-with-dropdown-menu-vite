e5ba8b2e08d5bc94e42412d9fa955a70e0db96f3
ArmyofDeux <103246856+Emmanuetoks@users.noreply.github.com>
2023-04-24 01:55:35 -0700

    Initial Commit
    Completed website 😁😁

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,162 @@
+import React from 'react'
+
+const Navbar = () => {
+    const showDropdownMenu = (e) => {
+        let dropdownLink = e.target.parentElement;
+        if (dropdownLink.getAttribute('dropdown-menu-visible') === 'true') {
+            dropdownLink.setAttribute('dropdown-menu-visible', 'false')
+            dropdownLink.style.color = 'var(--clr-gray)'
+        } else if (dropdownLink.getAttribute('dropdown-menu-visible') === 'false') {
+            dropdownLink.setAttribute('dropdown-menu-visible', 'true');
+            dropdownLink.style.color = 'black'
+        } else {
+            console.log('Why did you change my code');
+        }
+    }
+
+
+    const toggleMobMenu = () => {
+        let primaryNav = document.getElementById('primaryNav');
+        let bg = document.querySelector('.bg')
+        bg.classList.toggle('menu-is-active')
+        primaryNav.style.display = 'flex'
+        setTimeout(() => {
+            let showValue = primaryNav.getAttribute('is-mobile-menu');
+
+            if (showValue === 'true') {
+                primaryNav.setAttribute('is-mobile-menu', 'false')
+
+            } else if (showValue === 'false') {
+                primaryNav.setAttribute('is-mobile-menu', 'true')
+            } else {
+                console.log('dont change my code');
+            }
+
+        }, 0.00001);
+
+    }
+
+
+    return (
+        <nav className='navbar nav flex'>
+            <section className='nav__logo'>
+                <img src='/public/images/logo.svg' />
+            </section>
+            <section id='primaryNav' is-mobile-menu='false' className='primary-navigation flex'>
+                <ul className='nav__menu flex'>
+                    <li dropdown-menu-visible='false' id='features' className='pos-rel'>
+                        <a href='#' className='nav__link nav__link-dropdown capitalize'>
+                            Features
+                        </a>
+
+                        <button onClick={showDropdownMenu} className='dropdown-btn'>
+                            <a href='#' className='pos-rel sr-only'>
+                                dropdown button
+                            </a>
+                        </button>
+
+                        <div className='dropdown-navbar pos-abs'>
+                            <ul className='dropdown-menu grid grid-col-2'>
+                                <li>
+                                    <img src='/images/icon-todo.svg' />
+                                </li>
+
+                                <li >
+                                    <a href='#' className='capitalize dropdown-link'>todo list</a>
+                                </li>
+                                <li>
+                                    <img src='/images/icon-calendar.svg' />
+                                </li>
+
+                                <li >
+                                    <a href='#' className='capitalize dropdown-link'>calendar</a>
+                                </li>
+                                <li>
+                                    <img src='/images/icon-reminders.svg' />
+                                </li>
+
+                                <li >
+                                    <a href='#' className='capitalize dropdown-link'>reminders</a>
+                                </li>
+                                <li>
+                                    <img src='/images/icon-planning.svg' />
+                                </li>
+
+                                <li >
+                                    <a href='#' className='capitalize dropdown-link'>planning</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+
+                    <li dropdown-menu-visible='false' id='company' className='pos-rel'>
+                        <a href='#' className='nav__link nav__link-dropdown capitalize'>
+                            company
+                        </a>
+                        <button onClick={showDropdownMenu} className='dropdown-btn'>
+                            <a href='#' className='pos-rel sr-only'>
+                                dropdown button
+                            </a>
+                        </button>
+                        <div className='dropdown-navbar pos-abs'>
+                            <ul className='flex flex-col dropdown-menu flex-gap-1'>
+                                <li>
+                                    <a href='#' className='capitalize dropdown-link'>
+                                        history
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href='#' className='capitalize dropdown-link '>
+                                        <pre>our team</pre>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href='#' className='capitalize dropdown-link'>
+                                        blog
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <a href='#' className='nav__link capitalize'>
+                            careers
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#' className='nav__link capitalize'>
+                            about
+                        </a>
+                    </li>
+                </ul>
+
+
+
+                <div className='cta-container flex'>
+                    <a href='#' className='cta-link'>
+                        <btn className='cta-btn'>
+                            Login
+                        </btn>
+                    </a>
+
+                    <a href='#' className='cta-link'>
+                        <btn className='cta-btn'>
+                            Register
+                        </btn>
+                    </a>
+                </div>
+            </section>
+
+            <button onClick={toggleMobMenu} className='menu-btn'>
+                <span className='sr-only'>
+                    menu
+                </span>
+            </button>
+
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/pages/Home .jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import '../../CSS/index.css'
+
+const Home = () => {
+  return (
+    <div className='bg'>
+      <header>
+        <Navbar />
+      </header>
+
+      <main>
+        <div className='main-container grid '>
+          <section className='main-container__left-section flex flex-col'>
+            <article>
+              <h1 className='big-heading'>Make remote work</h1>
+              <p>
+                Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.
+              </p>
+
+              <a href='#'>
+              <button className='big-btn' >Learn more</button>
+            </a>
+            </article>
+            
+            <ul className='flex client-companies'>
+              <li>
+                <img src='/images/client-databiz.svg'/>
+                <span className='sr-only'>databiz</span>
+              </li>
+
+              <li>
+                <img src='/images/client-audiophile.svg'/>
+                <span className='sr-only'>audiophile</span>
+              </li>
+
+              <li>
+                <img src='/images/client-meet.svg'/>
+                <span className='sr-only'>meet</span>
+              </li>
+
+              <li>
+                <img src='/images/client-maker.svg'/>
+                <span className='sr-only'>maker</span>
+              </li>
+            </ul>
+          </section>
+          <section>
+            <picture>
+              <source className='hero-img' media='(max-width:900px)' srcSet='/images/image-hero-mobile.png' />
+              <img className='hero-img' src='/images/image-hero-desktop.png' />
+            </picture>
+          </section>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default Home 
